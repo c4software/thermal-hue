f0a11c57660e5cf6fdcd18a6c3afa81452e6cca6
Brosseau Valentin <c4software@gmail.com>
2017-06-04 16:47:38 +0200

    add structure

---
+++ db.py
@@ -0,0 +1,4 @@
+import sqlite3
+
+def init_db():
+    pass
--- main.py
+++ main.py
@@ -1,6 +1,18 @@
 import argparse
+import sqlite3
+from db import init_db
+
+try:
+    conn = sqlite3.connect('data.db')
+except e as Exception:
+    print ("Sqlite db can't be write")
+    exit()
 
 parser = argparse.ArgumentParser()
-parser.add_argument("--stdin", help="Output the current temperature value to the stdin")
-parser.add_argument("--init", help="Init the database")
-parser.parse_args()
+parser.add_argument("--stdin", action="store_true", help="Output the current temperature value to the stdin")
+parser.add_argument("--init", action="store_true", help="Init the database")
+args = parser.parse_args()
+
+if args.init:
+    init_db()
+    exit()
